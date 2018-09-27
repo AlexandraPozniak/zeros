6fc8d27de7579d9f08be731620dba3f9aacdbc9a
AlexandraPozniak <alexandra.poznyak1@gmail.com>
2018-09-27 22:12:02 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let res = 0;
+    while (number>0){
+      number /= 5;
+      res += Math.floor(number);
+
+  }
+  return res;
 }
